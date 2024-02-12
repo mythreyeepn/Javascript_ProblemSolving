242b82058dd21a557872ea9478816cd5969863d6
Mythreyee Pingala <mythreyeepn@gmail.com>
2024-02-12 12:18:49 -0600

    firstDuplicate

---
+++ firstDuplicate.js
@@ -0,0 +1,14 @@
+function findFirstRepeated(string) {
+  let array = string.split("");
+  let obj = {};
+  for (var i = 0; i < array.length; i++) {
+    if (obj[array[i]]) {
+      return array[i];
+    } else {
+      obj[array[i]] = 1;
+    }
+  }
+  return false;
+}
+
+console.log(findFirstRepeated("Abcdefgb"));
